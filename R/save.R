4a45ca88bda92a7145bac6e3d2b77e9a339997f2
Javier Luraschi <javierluraschi@hotmail.com>
2017-10-02 16:58:20 -0700

    add signature param to model save

--- R/save.R
+++ R/save.R
@@ -1,6 +1,23 @@
 #' @export
-tfserve_save <- function(model_path = "models/mnist", overwrite = FALSE) {
-  if (overwrite && dir.exists(model_path)) unlink(model_path, recursive = TRUE)
-  builder <- tf$saved_model$builder$SavedModelBuilder(model_path)
+tfserve_save <- function(
+  path = "models/mnist",
+  signature = NULL,
+  overwrite = FALSE)
+{
+
+  if (overwrite && dir.exists(path)) unlink(path, recursive = TRUE)
+  builder <- tf$saved_model$builder$SavedModelBuilder(path)
+
+  if (!is.null(signature)) {
+    builder$add_meta_graph_and_variables(
+      tf$Session(),
+      list(
+        tf$python$saved_model$tag_constants$SERVING
+      ),
+      signature_def_map = signature,
+      legacy_init_op = "legacy_init_op"
+    )
+  }
+
   builder$save()
 }

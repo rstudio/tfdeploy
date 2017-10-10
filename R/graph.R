15e3d688a68673ae68d10bf02b594b593206637f
Javier Luraschi <javierluraschi@hotmail.com>
2017-10-10 15:23:08 -0700

    need to use zero-based array index in protobof object

--- R/graph.R
+++ R/graph.R
@@ -1,24 +1,28 @@
 #' @export
-tfserve_view_graph <- function(path = file.path(getwd(), "trained/tensorflow-mnist/1/saved_model.pb")) {
+tfserve_view_graph <- function(
+  path_pb = file.path(getwd(), "trained/tensorflow-mnist/1/saved_model.pb"),
+  log_dir = file.path(getwd(), "logs")) {
+
   gfile <- tf$python$platform$gfile
   compat <- tf$python$util$compat
   saved_model_pb2 <- tf$core$protobuf$saved_model_pb2
 
   with(tf$Session() %as% sess, {
-    with(gfile$FastGFile(path, "rb") %as% f, {
+    with(gfile$FastGFile(path_pb, "rb") %as% f, {
       graph_def <- tf$GraphDef()
 
       data <- compat$as_bytes(f$read())
       sm <- saved_model_pb2$SavedModel()
       sm$ParseFromString(data)
 
-      # if (length(sm$meta_graphs) > 1) stop("More than one graph")
+      if (sm$meta_graphs$`__len__`() > 1) stop("More than one graph")
 
-      g_in <- tf$import_graph_def(sm$meta_graphs[[1]]$graph_def)
+      g_in <- tf$import_graph_def(sm$meta_graphs[[0]]$graph_def)
     })
+
+    train_writer <- tf$summary$FileWriter(log_dir)
+    train_writer$add_graph(sess$graph)
   })
 
-  LOGDIR <- "logs"
-  train_writer <- tf$summary$FileWriter(LOGDIR)
-  train_writer$add_graph(sess$graph)
+  tensorboard(log_dir = log_dir)
 }

82f36f955123c8d23a8d039b2d44aade3f59101b
Javier Luraschi <javierluraschi@hotmail.com>
2017-10-13 14:07:41 -0700

    basic httpuv server

--- R/serve.R
+++ R/serve.R
@@ -2,7 +2,27 @@
 #'
 #' Serve a TensorFlow Model into a local REST API.
 #'
+#' @importFrom httpuv runServer
 #' @export
-serve <- function(model_path) {
-
+serve <- function(model_path, host = "127.0.0.1", port = 8089) {
+  httpuv::runServer(host, port, list(
+      onHeaders = function(req) {
+        NULL
+      },
+      call = function(req){
+        list(
+          status = 500L,
+          headers = list(
+            "Content-Type" = "text/plain; charset=UTF-8"
+          ),
+          body = charToRaw(enc2utf8(
+            "Hello from tfserve."
+          ))
+        )
+      },
+      onWSOpen = function(ws) {
+        NULL
+      }
+    )
+  )
 }

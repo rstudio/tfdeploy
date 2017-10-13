fb79be0c6b5227cfd5353d4738aae1d7e2207fd9
Javier Luraschi <javierluraschi@hotmail.com>
2017-10-13 14:57:31 -0700

    regex handler support

--- R/serve.R
+++ R/serve.R
@@ -5,24 +5,65 @@
 #' @importFrom httpuv runServer
 #' @export
 serve <- function(model_path, host = "127.0.0.1", port = 8089) {
-  httpuv::runServer(host, port, list(
-      onHeaders = function(req) {
-        NULL
-      },
-      call = function(req){
-        list(
-          status = 500L,
-          headers = list(
-            "Content-Type" = "text/plain; charset=UTF-8"
-          ),
-          body = charToRaw(enc2utf8(
-            "Hello from tfserve."
-          ))
-        )
-      },
-      onWSOpen = function(ws) {
-        NULL
-      }
-    )
+  load_model(model_path)
+  run_server(host, port)
+}
+
+load_model <- function(model_path) {
+  tf$reset_default_graph()
+
+  graph <- tf$saved_model$loader$load(
+    tf$Session(),
+    list(tf$python$saved_model$tag_constants$SERVING),
+    model_path)
+
+  graph$signature_def
+}
+
+server_handlers <- function() {
+  list(
+    "^/[^/]*$" = function() {
+      list(
+        status = 500L,
+        headers = list(
+          "Content-Type" = "text/plain; charset=UTF-8"
+        ),
+        body = charToRaw(enc2utf8(
+          "Hello from tfserve."
+        ))
+      )
+    },
+    ".*" = function() {
+      list(
+        status = 404L,
+        headers = list(
+          "Content-Type" = "text/plain; charset=UTF-8"
+        ),
+        body = charToRaw(enc2utf8(
+          "Invalid Request."
+        ))
+      )
+    }
   )
+}
+
+run_server <- function(host, port) {
+  handlers <- server_handlers()
+
+  httpuv::runServer(host, port, list(
+    onHeaders = function(req) {
+      NULL
+    },
+    call = function(req){
+      matches <- sapply(names(handlers), function(e) grepl(e, req$PATH_INFO))
+      handlers[matches][[1]]()
+    },
+    onWSOpen = function(ws) {
+      NULL
+    }
+  ))
+}
+
+swagger_from_graph <- function() {
+  # http://petstore.swagger.io/v2/swagger.json
 }

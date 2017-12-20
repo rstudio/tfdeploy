946b444ff2242d14a298e651662d771559777d70
Javier Luraschi <javierluraschi@hotmail.com>
2017-12-20 08:34:35 -0800

    configure cloudml tests under travis

--- tests/testthat/test-predict-cloudml.R
+++ tests/testthat/test-predict-cloudml.R
@@ -1,6 +1,7 @@
 context("Predict CloudML")
 
 test_that("can predict mnist model from cloudml", {
+  if (!cloudml_tests_configured()) skip()
 
   results <- cloudml::cloudml_predict(
     jsonlite::read_json(

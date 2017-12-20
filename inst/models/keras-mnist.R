5f37a0136e7dfa26e77e1ccc61f72da75e758730
Javier Luraschi <javierluraschi@hotmail.com>
2017-12-19 23:45:10 -0800

    move training scripts from package into model examples

---
+++ inst/models/keras-mnist.R
@@ -0,0 +1,44 @@
+library(keras)
+
+mnist <- dataset_mnist()
+
+x_train <- mnist$train$x
+y_train <- mnist$train$y
+x_test <- mnist$test$x
+y_test <- mnist$test$y
+
+# reshape
+dim(x_train) <- c(nrow(x_train), 784)
+dim(x_test) <- c(nrow(x_test), 784)
+
+# rescale
+x_train <- x_train / 255
+x_test <- x_test / 255
+
+y_train <- to_categorical(y_train, 10)
+y_test <- to_categorical(y_test, 10)
+
+model <- keras_model_sequential()
+model %>%
+  layer_dense(units = 256, activation = 'relu', input_shape = c(784)) %>%
+  layer_dropout(rate = 0.4) %>%
+  layer_dense(units = 128, activation = 'relu') %>%
+  layer_dropout(rate = 0.3) %>%
+  layer_dense(units = 10, activation = 'softmax')
+
+model %>% compile(
+  loss = 'categorical_crossentropy',
+  optimizer = optimizer_rmsprop(),
+  metrics = c('accuracy')
+)
+
+model %>% fit(
+  x_train, y_train,
+  epochs = epochs, batch_size = 128,
+  validation_split = 0.2
+)
+
+model %>% keras::evaluate(x_test, y_test) %>% print()
+
+unlink("keras-mnist", recursive = TRUE)
+export_savedmodel(model, "keras-mnist")

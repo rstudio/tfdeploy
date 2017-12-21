1a4667afedbbc18cc750454f24ddb685cd3f1774
Javier Luraschi <javierluraschi@hotmail.com>
2017-12-20 19:00:43 -0800

    fix export prediction test

--- tests/testthat/test-predict-export.R
+++ tests/testthat/test-predict-export.R
@@ -2,10 +2,13 @@ context("Predict Exported SavedModel")
 
 test_that("can predict mnist model from a local file", {
   results <- predict_savedmodel(
-    list(images = list(rep(0, 784))),
+    list(list(images = rep(0, 784))),
     location = model_dir,
     service = "export"
   )
 
-  expect_true(ncol(results[[1]]) == 10)
+  expect_equal(
+    length(results$predictions$scores[[1]]),
+    10
+  )
 })

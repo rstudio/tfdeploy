76e9e876484aebcfab9d35b3544dabf725a97514
Javier Luraschi <javierluraschi@hotmail.com>
2017-12-26 15:22:04 -0800

    fix travis troubleshoot script

--- .travis.R
+++ .travis.R
@@ -1,5 +1,5 @@
 parent_dir <- dir("../", full.names = TRUE)
-package <- parent_dir[grepl("cloudml_", parent_dir)]
+package <- parent_dir[grepl("tfdeploy_", parent_dir)]
 install.packages(package, repos = NULL, type = "source")
 
 source("testthat.R")

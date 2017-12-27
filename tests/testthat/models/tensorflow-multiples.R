3336489c8c0020fa6dd0a0c924230daf762e7554
Javier Luraschi <javierluraschi@hotmail.com>
2017-12-26 22:26:07 -0800

    add test with multiple input output tensors

---
+++ tests/testthat/models/tensorflow-multiples.R
@@ -0,0 +1,18 @@
+library(tensorflow)
+
+# define output folder
+model_dir <- tempfile()
+
+# define simple string-based tensor operations
+sess <- tf$Session()
+input1 <- tf$placeholder(tf$string)
+input2 <- tf$placeholder(tf$string)
+output1 <- tf$string_join(inputs = c("Input1: ", input1, "!"))
+output2 <- tf$string_join(inputs = c("Input2: ", input2, "!"))
+
+unlink("tensorflow-multiple", recursive = TRUE)
+export_savedmodel(
+  sess,
+  "tensorflow-multiple",
+  inputs = list(i1 = input1, i2 = input2),
+  outputs = list(o1 = output1, o2 = output2))

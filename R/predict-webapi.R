4009e87e4e1265962a0a66aac6e6497b1e59e540
Javier Luraschi <javierluraschi@hotmail.com>
2017-12-29 20:41:33 -0800

    implement predict_savedmodel for webapis

--- R/predict-webapi.R
+++ R/predict-webapi.R
@@ -3,6 +3,15 @@ predict_savedmodel.webapi_predictionservice <- function(
   instances,
   model,
   signature_name = tf$saved_model$signature_constants$DEFAULT_SERVING_SIGNATURE_DEF_KEY,
+  url = "http://127.0.0.1:8089/api/serving_default/predict/",
   ...) {
+
+  httr::POST(
+    url = url,
+    body = list(
+      instances = instances
+    ),
+    encode = "json"
+  ) %>% httr::content(as = "text") %>% jsonlite::fromJSON()
 
 }

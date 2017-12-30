7a9ab82fa97109bb2fbdaa6b33dcd46f205765a0
Javier Luraschi <javierluraschi@hotmail.com>
2017-12-29 20:41:13 -0800

    add test to validate serve_savedmodel() against existing models

---
+++ tests/testthat/test-predict-serve.R
@@ -0,0 +1,69 @@
+context("Predict Serve Model")
+
+test_serve_predict <- function(instances, model) {
+  full_path <- normalizePath(model)
+
+  output_log <- tempfile()
+
+  process <- processx::process$new(
+    command = "RScript",
+    args = c(
+      "-e",
+      paste0(
+        "library(tfdeploy); ",
+        "serve_savedmodel('",
+        full_path,
+        "')"
+      ),
+      "--vanilla"
+    ),
+    stdout = output_log
+  )
+
+  Sys.sleep(3)
+
+  results <- predict_savedmodel(
+    instances,
+    type = "webapi")
+
+  process$kill()
+
+  expect_true(!is.null(results$predictions))
+  expect_true(!is.null(results$predictions[[1]]))
+}
+
+test_that("can predict tensorflow mnist model in local serve", {
+  test_serve_predict(
+    list(
+      list(images = rep(0, 784))
+    ),
+    system.file("models/tensorflow-mnist", package = "tfdeploy")
+  )
+})
+
+test_that("can predict keras mnist model in local serve", {
+  test_serve_predict(
+    list(
+      list(dense_1_input = rep(0, 784))
+    ),
+    "models/keras-mnist"
+  )
+})
+
+test_that("can predict tensorflow with multiple tensors model in local serve", {
+  test_serve_predict(
+    list(
+      list(i1 = "One", i2 = "Two"),
+      list(i1 = "One", i2 = "Two")
+    ),
+    "models/tensorflow-multiple")
+})
+
+test_that("can predict keras with multiple tensors model in local serve", {
+  test_serve_predict(
+    list(
+      list(input1 = "a", input2 = "b"),
+      list(input1 = "a", input2 = "b")
+    ),
+    "models/keras-multiple")
+})

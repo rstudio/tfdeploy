009a35a1216ae455bc20f0d351bcc8cca5cfb00d
Javier Luraschi <javierluraschi@hotmail.com>
2018-01-02 19:05:22 -0800

    fix tfestimators model example

--- tests/testthat/models/tfestimators-mtcars.R
+++ tests/testthat/models/tfestimators-mtcars.R
@@ -1,7 +1,11 @@
 library(tfestimators)
 
-mtcars_input_fn <- function(data) {
-  input_fn(data, features = c("disp", "cyl"), response = "mpg")
+mtcars_input_fn <- function(data, num_epochs = 1) {
+  input_fn(data,
+           features = c("disp", "cyl"),
+           response = "mpg",
+           batch_size = 32,
+           num_epochs = num_epochs)
 }
 
 cols <- feature_columns(column_numeric("disp"), column_numeric("cyl"))
@@ -12,7 +16,7 @@ indices <- sample(1:nrow(mtcars), size = 0.80 * nrow(mtcars))
 train <- mtcars[indices, ]
 test  <- mtcars[-indices, ]
 
-model %>% train(mtcars_input_fn(train))
+model %>% train(mtcars_input_fn(train, num_epochs = 10))
 
 unlink("tfestimators-mtcars", recursive = TRUE)
 export_savedmodel(model, "tfestimators-mtcars")

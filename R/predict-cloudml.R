09207fa42c68e0638228a49b84bd0d5f74957fdb
Javier Luraschi <javierluraschi@hotmail.com>
2018-01-04 20:58:34 -0800

    remove gcloud since it was removed from cloudml

--- R/predict-cloudml.R
+++ R/predict-cloudml.R
@@ -4,7 +4,6 @@ predict_savedmodel.cloudml_predictionservice <- function(
   model = NULL,
   signature_name = "serving_default",
   version = NULL,
-  gcloud = NULL,
   ...) {
-  cloudml::cloudml_predict(instances, name = model, version = version, gcloud = gcloud)
+  cloudml::cloudml_predict(instances, name = model, version = version)
 }

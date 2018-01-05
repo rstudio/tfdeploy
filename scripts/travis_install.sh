959e07a80affcfb66f93f56c06546229bebd0484
Javier Luraschi <javierluraschi@hotmail.com>
2018-01-05 15:05:13 -0800

    add travis_install.sh

---
+++ scripts/travis_install.sh
@@ -0,0 +1,12 @@
+pip2.7 install --upgrade --ignore-installed --user travis pip setuptools wheel virtualenv
+
+if [[ "$TF_VERSION" == "1.3" ]]; then
+      echo "Installing TensorFlow v1.3 ...";
+      Rscript -e 'tensorflow::install_tensorflow(version = "1.3")';
+elif [[ "$TF_VERSION" == "1.4" ]]; then
+      echo "Installing TensorFlow v1.4 ...";
+      Rscript -e 'tensorflow::install_tensorflow(version = "1.4")';
+elif [[ "$TF_VERSION" == "nightly" ]]; then
+      echo "Installing TensorFlow nightly ...";
+      Rscript -e 'tensorflow::install_tensorflow(version = "nightly")';
+fi

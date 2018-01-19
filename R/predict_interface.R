75f1b44b1499021514964544c9fb5ef68dcec43c
Javier Luraschi <javierluraschi@hotmail.com>
2018-01-19 12:16:51 -0800

    remove cloudml from interface

--- R/predict_interface.R
+++ R/predict_interface.R
@@ -1,37 +1,31 @@
 #' Predict using a SavedModel
 #'
-#' Runs a prediction over a saved model file, local service or cloudml model.
+#' Runs a prediction over a saved model file, web API or graph object.
 #'
 #' @inheritParams predict_savedmodel
 #'
 #' @param instances A list of prediction instances to be passed as input tensors
 #'   to the service. Even for single predictions, a list with one entry is expected.
 #'
-#' @param model The model as a local path, a REST url, CloudML name or graph object.
+#' @param model The model as a local path, a REST url or graph object.
 #'
 #'   A local path can be exported using \code{export_savedmodel()}, a REST URL
-#'   can be created using \code{serve_savedmodel()}, a CloudML model can be deployed
-#'   usin \code{cloudml::cloudml_deploy()} and a graph object loaded using
+#'   can be created using \code{serve_savedmodel()} and a graph object loaded using
 #'   \code{load_savedmodel()}.
 #'
-#'   Notice that predicting over a CloudML model requires a \code{version}
-#'   parameter to identify the model.
-#'
 #'   A \code{type} parameter can be specified to explicitly choose the type model
-#'   performing the prediction. Valid values are \code{cloudml}, \code{export},
-#'   \code{webapi} and \code{graph}.
+#'   performing the prediction. Valid values are \code{export}, \code{webapi} and
+#'   \code{graph}.
 #'
 #' @param ... See [predict_savedmodel.export_prediction()],
 #'   [predict_savedmodel.graph_prediction()],
-#'   [predict_savedmodel.webapi_prediction()]
-#'   and [predict_savedmodel.cloudml_prediction()] for additional options.
+#'   [predict_savedmodel.webapi_prediction()] for additional options.
 #'
 #' #' @section Implementations:
 #'
 #'   - [predict_savedmodel.export_prediction()]
 #'   - [predict_savedmodel.graph_prediction()]
 #'   - [predict_savedmodel.webapi_prediction()]]
-#'   - [predict_savedmodel.cloudml_prediction()]
 #'
 #' @seealso [export_savedmodel()], [serve_savedmodel()], [load_savedmodel()]
 #'
@@ -58,8 +52,6 @@ predict_savedmodel <- function(
     type <- "graph"
   else if (grepl("https?://", model))
     type <- "webapi"
-  else if ("version" %in% names(params))
-    type <- "cloudml"
   else
     type <- "export"
 

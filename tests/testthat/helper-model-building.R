950c9656949cc6af4d0b643536f606bfa2a7be5a
Daniel Falbel <dfalbel@gmail.com>
2026-04-28 16:38:21 -0300

    Use full path to Rscript in tests
    R CMD check --as-cran requires using the full path to Rscript
    via R.home('bin') instead of bare 'Rscript'.

--- tests/testthat/helper-model-building.R
+++ tests/testthat/helper-model-building.R
@@ -22,7 +22,7 @@ for (model in models) {
   message("Running ", model)
 
   p <- processx::process$new(
-    command = "Rscript",
+    command = file.path(R.home("bin"), "Rscript"),
     args = paste0("model-building/", model, ".R"), stderr = "|", stdout = "|",
     wd = getwd()
     )
